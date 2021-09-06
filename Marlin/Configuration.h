6345647d3dff222562505e97f1acc5b75fce5217
itsjakeman <jakecgarcia@gmail.com>
2021-09-06 17:46:45 -0500

    Added PETG preheat profile

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1879,6 +1879,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 245
+#define PREHEAT_2_TEMP_BED     65
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
